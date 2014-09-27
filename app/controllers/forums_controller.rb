04067b1420910f818d5b9c5af728eb0b6867dedb
Zachary Meyer <wif.temp@gmail.com>
2014-09-27 15:25:50 -0500

    Board creation added

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,5 +1,17 @@
+include ForumsHelper
+
 class ForumsController < ApplicationController
 	def index
 		@forums = Forum.all
+	end
+
+	def new
+		@forum = Forum.new
+	end
+
+	def create
+		@forum = Forum.new(forum_params)
+		@forum.save
+		redirect_to '/forums'
 	end
 end
