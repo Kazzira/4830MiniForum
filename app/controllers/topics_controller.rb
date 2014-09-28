5871336495591fd7835bd46f4096573762004b26
Zachary Meyer <wif.temp@gmail.com>
2014-09-27 19:50:47 -0500

    Fix redirect topic creation

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,7 +12,7 @@ class TopicsController < ApplicationController
 		@post = Post.new(user: params["user_name"], body: (params["body"]), topic: @topic)
 		@post.save
 		@topic.save
-		redirect_to topics_path(@topic)
+		redirect_to topic_path(@topic.id)
 	end
 
 	def show
